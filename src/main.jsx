876a46879adf6e11875225cd32d9b3e26bf9cc11
Muneeb381a <muneebafzal381a@gmail.com>
2024-10-03 15:58:36 +0500

    create frontend application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import NotesProvider from './context/Notes.context'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import UpdatePage from './Update/index.tsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const routes = createBrowserRouter([
+  {
+    path:'/',
+    Component:App,
+  },
+  {
+    path: '/update/:id',
+    Component: UpdatePage,
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <NotesProvider>
+    {/* <App /> */}
+    <RouterProvider router={routes} />
+  </NotesProvider>,
 )
